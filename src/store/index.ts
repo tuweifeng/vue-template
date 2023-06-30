dfd1dc42867842e359ffe223caf2109e30bd435a
tuweifeng <907391489@qq.com>
2023-06-30 15:58:09 +0800

    add：状态持久化
       保存在本地，刷新重新读取

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,21 @@
 import { createStore } from 'vuex'
 
+const init_formcache = ()=>{
+  const cache =  JSON.parse(localStorage.getItem("formcache") || "[]")
+  console.log("formcache", cache)
+  return cache
+} 
+
 export default createStore({
   state: {
+    formcache: init_formcache() as {key: string, label:string, data:any}[]
   },
   getters: {
   },
   mutations: {
+    update_formcache(state, formcache){
+      state.formcache = formcache
+    }
   },
   actions: {
   },
